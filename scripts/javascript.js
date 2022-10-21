fe282034ed535d7bd920c8cea0420deb16c66e41
VitorDanielA <vitordanii87@gmail.com>
2022-10-21 19:51:27 -0300

    Arrumando o JavaScript da Homescreen

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,12 +1,12 @@
 const sidebar = document.getElementById('sidebar');
 const closeopen = document.getElementById('closeopen');
 
+closeopen.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
+
 var user = getUser();
 console.log('User ', user.nome)
 var val = document.getElementById('username')
 console.log(val)
 val.innerText = user.nome
-
-closeopen.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-});
