f6705bf6480f894b14e37ac341fe318bcf9d31ba
VitorDanielA <vitordanii87@gmail.com>
2022-11-07 13:05:21 -0300

    Esqueleto inicial da Requisição

---
+++ js/requisicao.js
@@ -0,0 +1,36 @@
+function stopPropagation(event){
+    event.stopPropagation();
+}
+
+function openAddPopup(){
+    popupAdd.classList.add("openAddPopup");
+}
+
+function closeAddPopup(){
+    popupAdd.classList.remove("openAddPopup");
+}
+
+function openPopup(id){
+    this.selectedId = id
+    popup.classList.add("open_popup");
+}
+
+function teladisabled(){
+    telaDesativada.classList.add("disabled_tela");
+    backdrop.classList.add("disabled_tela");
+}
+
+function getindex(x){
+    globalThis.Index = x.rowIndex;
+}
+
+function telaEnabled(){
+    telaDesativada.classList.remove("disabled_tela");
+    backdrop.classList.remove("disabled_tela");
+}
+
+var tablee = document.getElementById("itens-table");
+
+function closePopup(){
+    popup.classList.remove("open_popup");
+}
