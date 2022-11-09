ca73bc639056d39e0e1873520dd640770454cbde
VitorDanielA <vitordanii87@gmail.com>
2022-11-09 18:45:56 -0300

    Fazendo ligação com o backend e adicionando detalhes

--- js/requisicao.js
+++ js/requisicao.js
@@ -1,3 +1,78 @@
+var selectedId
+var requisicaoList = [] 
+var requisicao = {}
+
+function requisicaoAddChange(){
+    requisicao.nome = document.getElementById('qtdReq').value;
+    console.log(requisicao);
+}
+
+atualizarTabela()
+function atualizarTabela(){
+    get('requisicao').then(data=>{
+    console.log('Data', data)
+    this.requisicaoList = data
+    this.tableCreate(this.requisicaoList)
+        }).catch(error=>{
+        console.log('Error ', error)
+    })
+}
+
+function tableCreate(data){
+    var tableBody = document.getElementById('table-body');
+    if(tableBody){
+        tableBody.innerHTML = ''
+         data.forEach(element => {
+        var row = document.createElement("tr");
+        
+        var colNumReq = document.createElement("td")
+        colNumReq.appendChild(document.createTextNode(element.id))
+        row.appendChild(colNumReq)
+
+        var colUsuReq = document.createElement("td")
+        colUsuReq.appendChild(document.createTextNode(element.usuarioRequisitante))
+        row.appendChild(colUsuReq)
+
+        var colDescricao = document.createElement("td")
+        colDescricao.appendChild(document.createTextNode(element.setor))
+        row.appendChild(colDescricao)
+        
+        tableBody.appendChild(row)
+
+        var colInfo = document.createElement("td")
+        colInfo.setAttribute("onclick", "openPopup("+element.id+")")
+        var infoLink = document.createElement("a")
+        var imgInfo = document.createElement("img")
+        imgInfo.setAttribute("src", "images/simbolo-de-informacao.png")
+        infoLink.appendChild(imgInfo)
+
+        colInfo.appendChild(infoLink)
+        row.appendChild(colInfo)
+
+        var colRemover = document.createElement("td")
+        colRemover.setAttribute("onclick", "openPopup("+element.id+")")
+        var removerLink = document.createElement("a")
+        var imgRemove = document.createElement("img")
+        imgRemove.setAttribute("src", "images/excluir2.png")
+        removerLink.appendChild(imgRemove)
+        
+        colRemover.appendChild(removerLink)
+        row.appendChild(colRemover)
+        
+        var colEditar = document.createElement("td")
+        colEditar.setAttribute("onclick", "openEditPopup("+element.id+")")
+        var editarLink = document.createElement("a")
+        var imgEditar = document.createElement("img")
+        imgEditar.setAttribute("src", "images/botao-editar2.png")
+        editarLink.appendChild(imgEditar)
+        
+        colEditar.appendChild(editarLink)
+        row.appendChild(colEditar)
+
+    });
+    }
+}
+
 function stopPropagation(event){
     event.stopPropagation();
 }
