b2a05c04b92e815e00cdf27e18efd355b045f3aa
VitorDanielA <vitordanii87@gmail.com>
2022-12-09 04:17:31 -0300

    refactor nas pastas e alterações no codigo, css, html e javascript em alguns locais

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,6 +1,9 @@
 const sidebar = document.getElementById('sidebar');
 const closeopen = document.getElementById('closeopen');
 
+closeopen.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
 var user = getUser();
 
 var val = document.getElementById('user_name')
